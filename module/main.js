c1ee544b1c151a6cf7f7a841e258262c13534245
midespinas <edgargana86@gmail.com>
2026-03-09 15:19:28 +0100

    Update main.js

--- module/main.js
+++ module/main.js
@@ -1,85 +1,93 @@
 const MODULE_ID = "ultima-emision";
 
-let panel;
+class RadioPanel extends Application {
 
-
-// INIT
-Hooks.once("init", () => {
-
-game.settings.register(MODULE_ID,"frecuencias",{
-name:"Frecuencias",
-scope:"world",
-config:false,
-type:Number,
-default:6
+static get defaultOptions() {
+return mergeObject(super.defaultOptions,{
+id:"radio-panel",
+title:"📻 La Última Emisión",
+width:720,
+height:"auto",
+resizable:true,
+template:null
 });
-
-});
-
-
-// READY
-Hooks.once("ready",()=>{
-
-console.log("Ultima Emision lista");
-
-setTimeout(()=>{
-abrirPanel();
-},300);
-
-});
-
-
-// MENSAJES
-function emitirMensaje(texto){
-
-ChatMessage.create({
-content:`<b>📻 ${texto}</b>`
-});
-
-const pantalla=document.querySelector("#radio-screen");
-
-if(pantalla){
-pantalla.innerHTML+=`<div>> ${texto}</div>`;
-pantalla.scrollTop=pantalla.scrollHeight;
 }
 
-}
+getData(){
 
+let frecuencias = game.settings.get(MODULE_ID,"frecuencias");
 
-// CREAR LUCES
-function generarLuces(){
-
-let frecuencias=game.settings.get(MODULE_ID,"frecuencias");
-
-let luces="";
+let luces=[];
 
 for(let i=0;i<6;i++){
+luces.push(i<frecuencias);
+}
 
-let estado=i<frecuencias?"on":"off";
+return {frecuencias,luces};
 
-luces+=`
+}
+
+activateListeners(html){
+
+super.activateListeners(html);
+
+html.find("#perder").click(async()=>{
+
+let f = game.settings.get(MODULE_ID,"frecuencias");
+
+if(f>0){
+
+f--;
+
+await game.settings.set(MODULE_ID,"frecuencias",f);
+
+emitirMensaje("Una frecuencia se ha perdido");
+
+this.render();
+
+}
+
+});
+
+html.find("#interferencia").click(()=>{
+
+emitirMensaje("La señal se llena de estática");
+
+});
+
+html.find("#reset").click(async()=>{
+
+await game.settings.set(MODULE_ID,"frecuencias",6);
+
+emitirMensaje("La señal vuelve a estabilizarse");
+
+this.render();
+
+});
+
+}
+
+_renderInner(){
+
+let data=this.getData();
+
+let lucesHTML="";
+
+data.luces.forEach(l=>{
+lucesHTML+=`
 <img class="radio-light"
-src="modules/ultima-emision/assets/light-${estado}.webp">
+src="modules/ultima-emision/assets/light-${l?"on":"off"}.webp">
 `;
+});
 
-}
-
-return luces;
-
-}
-
-
-// PANEL
-function abrirPanel(){
-
-let contenido=`
+let html=`
 
 <div class="radio-console">
 
 <h2 class="radio-title">LA ÚLTIMA EMISIÓN</h2>
 
-<div class="radio-lights" id="radio-lights">
-${generarLuces()}
+<div class="radio-lights">
+${lucesHTML}
 </div>
 
 <div id="radio-screen" class="radio-screen">
@@ -100,71 +108,49 @@ ${generarLuces()}
 
 `;
 
-panel=new Dialog({
-
-title:"📻 La Última Emisión",
-
-content:contenido,
-
-render:html=>{
-
-html.find("#perder").click(async()=>{
-
-let f=game.settings.get(MODULE_ID,"frecuencias");
-
-if(f>0){
-
-f--;
-
-await game.settings.set(MODULE_ID,"frecuencias",f);
-
-emitirMensaje("Una frecuencia se ha perdido");
-
-actualizarPanel();
+return $(html);
 
 }
 
-});
-
-
-html.find("#interferencia").click(()=>{
-
-emitirMensaje("La señal se llena de estática");
-
-});
-
-
-html.find("#reset").click(async()=>{
-
-await game.settings.set(MODULE_ID,"frecuencias",6);
-
-emitirMensaje("La señal vuelve a estabilizarse");
-
-actualizarPanel();
-
-});
-
 }
 
-},{
-width:720,
-resizable:true
+let panel;
+
+
+// INIT
+Hooks.once("init",()=>{
+
+game.settings.register(MODULE_ID,"frecuencias",{
+scope:"world",
+config:false,
+type:Number,
+default:6
 });
+
+});
+
+
+// READY
+Hooks.once("ready",()=>{
+
+panel = new RadioPanel();
 
 panel.render(true);
 
-}
+});
 
 
-// ACTUALIZAR PANEL SIN RECREARLO
-function actualizarPanel(){
+// MENSAJES
+function emitirMensaje(texto){
 
-const luces=document.querySelector("#radio-lights");
+ChatMessage.create({
+content:`<b>📻 ${texto}</b>`
+});
 
-if(luces){
+const pantalla=document.querySelector("#radio-screen");
 
-luces.innerHTML=generarLuces();
-
+if(pantalla){
+pantalla.innerHTML+=`<div>> ${texto}</div>`;
 }
 
 }
